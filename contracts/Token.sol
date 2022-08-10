290cf08c41d3dc1af869f7471c20ecb25bc74ded
ShvdeLub <lubin.antheaume@gmail.com>
2022-08-10 08:51:14 -0700

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,6 +14,7 @@ contract Token {
 
     string private _name;
     string private _symbol;
+    address empty = '0x000000000000000000';
 
      constructor(string memory name_, string memory symbol_) {
         _name = name_;
@@ -29,10 +30,10 @@ contract Token {
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return 8;
+        return 18;
     }
 
-    function totalSuply() public view returns(uint8) {
+    function totalSuply() public view returns(uint256) {
         return _totalSupply;
     }
 
@@ -41,22 +42,49 @@ contract Token {
     }
 
     function transfer(address _to, uint256 amount) public returns(uint256 balance) {
+        require(_to !== empty, "cannot send token to an empty address");
+        require(_balances[_msgSender()] >== amount, "the sender don't have enough tokens")
         address owner = _msgSender();
         _transfer(owner, _to, amount);
         return true;
     }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
+    }
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender !== empty,"cannot send token from an empty address");
+        address owner = _msgSender();
+        _approve(owner, _spender, _value);
+        return true
+    }
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_from !== empty,"transfer need to come from a real wallet");
+        require(_to !== empty,"transfer need to come from a real wallet");
+        require(allowance(_from, _msgSender()) >= _value,"the caller don't have allowance or the amount is too low")
         address spender = _msgSender();
         _spendAllowance(_from, spender, _value);
         _transfer(_from, _to, _value);
         return true;
     }
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        require(spender !== empty, "cannot increase allowance for an empty address")
         address owner = _msgSender();
-        _approve(owner, _spender, _value);
-        return true
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
     }
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return _allowances[_owner][_spender];
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        require(spender !== empty, "cannot increase allowance for an empty address")
+        require(allowance(_msgSender(), spender) >= subtractedValue,"the spender don't have allowance or the value is too low")
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(owner, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
     }
 }
